ad90f470b365a5c01cac508aa6f036fad9401f70
Bruno Ruas <bruno.ruas2@gmail.com>
2022-09-03 18:21:55 -0400

    first module upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Coleta de itens na nota fiscal SEFAZ AM
+
+hello world in python module building
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
--- src/modules/popUpImg.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-
-def showPopUp(path):
-    ImageItself = Image.open(path)
-    ImageNumpyFormat = np.asarray(ImageItself)
-    plt.imshow(ImageNumpyFormat)
-    plt.show()
